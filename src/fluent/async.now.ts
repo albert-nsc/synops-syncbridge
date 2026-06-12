233c9e4d5078b62d438d9a46aa38706f5bdb8142
Albert Minnie <140478972+albert-nsc@users.noreply.github.com>
2026-06-12 11:27:08 +0200

    restore previous script

--- src/fluent/async.now.ts
+++ src/fluent/async.now.ts
@@ -1,6 +1,6 @@
 // src/fluent/async.now.ts
 import { ScriptAction } from "@servicenow/sdk/core";
-// import { processLongRequest } from "../server/scriptActions/processLongRequest";
+import { processLongRequest } from "../server/processLongRequest";
 
 ScriptAction({
     $id: Now.ID["process-long-request-action"],
@@ -9,18 +9,5 @@ ScriptAction({
     description: "Processes queued long-running outbound requests",
     eventName: "x_nscgg_syncbridge.long_request.process",
     order: 100,
-    script: `
-        gs.info("[AsyncJobTest] INLINE SCRIPT ACTION HIT");
-
-        if (current) {
-            gs.info("[AsyncJobTest] current table = " + current.getTableName());
-            gs.info("[AsyncJobTest] current sys_id = " + current.getUniqueValue());
-
-            current.setValue("state", "processing");
-            current.setValue("error", "Inline Script Action fired");
-            current.update();
-        } else {
-            gs.error("[AsyncJobTest] current is not available");
-        }
-    `,
+    script: processLongRequest,
 });
