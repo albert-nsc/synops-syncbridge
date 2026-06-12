28a0ca68b74fba7f91bd62dadaf118a71e150548
Albert Minnie <140478972+albert-nsc@users.noreply.github.com>
2026-06-12 11:18:19 +0200

    temp test

--- src/fluent/async.now.ts
+++ src/fluent/async.now.ts
@@ -9,5 +9,18 @@ ScriptAction({
     description: "Processes queued long-running outbound requests",
     eventName: "x_nscgg_syncbridge.long_request.process",
     order: 100,
-    script: processLongRequest,
+    script: `
+        gs.info("[AsyncJobTest] INLINE SCRIPT ACTION HIT");
+
+        if (current) {
+            gs.info("[AsyncJobTest] current table = " + current.getTableName());
+            gs.info("[AsyncJobTest] current sys_id = " + current.getUniqueValue());
+
+            current.setValue("state", "processing");
+            current.setValue("error", "Inline Script Action fired");
+            current.update();
+        } else {
+            gs.error("[AsyncJobTest] current is not available");
+        }
+    `,
 });
