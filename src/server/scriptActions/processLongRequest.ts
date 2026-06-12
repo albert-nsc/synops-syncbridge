0c315a03779197b32c8a28a812e4d2290efda597
Albert Minnie <140478972+albert-nsc@users.noreply.github.com>
2026-06-12 10:29:33 +0200

    change back since the 'event' keyword is forbidden

--- src/server/scriptActions/processLongRequest.ts
+++ src/server/scriptActions/processLongRequest.ts
@@ -2,7 +2,6 @@ import { GlideRecord, gs } from "@servicenow/glide";
 import { RESTMessageV2 } from "@servicenow/glide/sn_ws";
 
 declare const current: any;
-declare const event: any;
 
 type NamedRESTMessageV2Ctor = new (
     name: string,
@@ -17,18 +16,12 @@ function createRestMessage(name: string, methodName: string): RESTMessageV2 {
 }
 
 export function processLongRequest() {
-    gs.info(`[AsyncJobTest] SCRIPT ACTION HIT`);
-    gs.info(`[AsyncJobTest] event.name=${event.name}`);
-    gs.info(`[AsyncJobTest] event.parm1=${event.parm1}`);
-    gs.info(`[AsyncJobTest] event.parm2=${event.parm2}`);
+    const job = current as GlideRecord;
 
-    const jobSysId = String(event.parm1 || "");
-    const job = new GlideRecord("x_nscgg_syncbridge_async_job");
+    const jobSysId = job.getUniqueValue();
+    const target = job.getValue("target") || "";
 
-    if (!job.get(jobSysId)) {
-        gs.error(`[AsyncJobTest] Could not find job from parm1: ${jobSysId}`);
-        return;
-    }
+    gs.info(`[AsyncJobTest] SCRIPT ACTION HIT. job=${jobSysId}, target=${target}`);
 
     job.setValue("state", "processing");
     job.setValue("error", "Script Action fired successfully");
